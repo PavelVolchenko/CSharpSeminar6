dc3a76cfbe68b2f6f3525320b098ba8ad4935634
Pavel Volchenko <2793553@gmail.com>
2022-10-05 04:59:00 +0300

    Исправил задачу 42

--- Task042/Program.cs
+++ Task042/Program.cs
@@ -10,24 +10,18 @@ int convertToString(int number)
     int remains = number % 2;
     int temp = number / 2;
     int result = remains;
-    Console.WriteLine(result);
-    while (remains != 0)
+    while (temp != 0)
     {
         remains = temp % 2;
         temp = temp / 2;
         count = count * 10;
         result += remains * count;
-        Console.WriteLine(result);
     }
     return result;
 }
-
-
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int bite = convertToString(number);
 Console.Write(bite);
-// string binaryCode = Convert.ToString(number, 2);
-// Console.Write(binaryCode);
